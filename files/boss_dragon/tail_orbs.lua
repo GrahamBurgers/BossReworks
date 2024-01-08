19316e2b8ec32c3878b6a6ddb6286b3ced384232
Graham <catface238@gmail.com>
2024-01-08 01:42:26 -0500

    tail orbs too fast

--- files/boss_dragon/tail_orbs.lua
+++ files/boss_dragon/tail_orbs.lua
@@ -1,12 +1,7 @@
-dofile_once("data/scripts/lib/utilities.lua")
-
 local entity_id    = GetUpdatedEntityID()
 local pos_x, pos_y = EntityGetTransform(entity_id)
 
--- check that we're only shooting every 10 frames
-if script_wait_frames(entity_id, 10) then return end
-
 dofile_once("mods/boss_reworks/files/projectile_utils.lua")
-CircleShot(entity_id, "mods/boss_reworks/files/boss_dragon/tail_orbs.xml", 6, pos_x, pos_y, 160)
+CircleShot(entity_id, "mods/boss_reworks/files/boss_dragon/tail_orbs.xml", 6, pos_x, pos_y, 110)
 
 GamePlaySound("data/audio/Desktop/projectiles.bank", "projectiles/orb_dragon/create", pos_x, pos_y)
