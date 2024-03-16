4fa00707f7b1d178f6bda3313af71e1f69ad5f59
Graham <catface238@gmail.com>
2024-03-16 19:41:15 -0400

    spell is functional!

--- files/spells/reward_master.lua
+++ files/spells/reward_master.lua
@@ -1,8 +1,10 @@
 local me = GetUpdatedEntityID()
 local x, y = EntityGetTransform(me)
-local radius = 18
+local radius = 12
 
-local variants = dofile_once("mods/boss_reworks/files/spells/master_spell_list.lua")
+Variants = dofile_once("mods/boss_reworks/files/spells/master_spell_list.lua")
+dofile_once("data/scripts/gun/gun_actions.lua")
+Actions = actions or {}
 
 local spells = EntityGetInRadiusWithTag(x, y, radius, "card_action") or {}
 local valid = {}
@@ -28,11 +30,48 @@ for i = 1, #spells do
     end
 end
 table.remove(valid, #valid) -- hax?
-if #valid >= 1 then
+
+local bonk = false
+if #valid >= 1 and #Actions > 0 then
     local entity = valid[1][1]
-    local x2, y2 = EntityGetTransform(entity)
-    GamePlaySound("data/audio/Desktop/items.bank", "magic_wand/mana_fully_recharged", x2, y2)
-else
+    local id = valid[1][3]
+    local pool = {}
+    bonk = true
+    for i = 1, #Variants do
+        for j = 1, #Variants[i] do
+            if Variants[i][j] == id then
+                pool = Variants[i]
+                break
+            end
+        end
+    end
+    if #pool > 0 then
+        local new_pool = {}
+        local which = 0
+        for i = 1, #pool do
+            for j = 1, #Actions do
+                if not (Actions[j].spawn_requires_flag and not HasFlagPersistent(Actions[j].spawn_requires_flag)) then
+                    new_pool[#new_pool+1] = pool[i]
+                    if pool[i] == id then
+                        which = #new_pool
+                    end
+                    break
+                end
+            end
+        end
+        if #new_pool > 0 then
+            local x2, y2 = EntityGetTransform(entity)
+            bonk = false
+            EntityKill(entity)
+            SetRandomSeed(GameGetFrameNum() + entity, 203458 + #new_pool + GameGetFrameNum())
+            GamePlaySound("data/audio/Desktop/items.bank", "magic_wand/mana_fully_recharged", x2, y2)
+            EntityLoad("data/entities/particles/poof_green.xml", x2, y2)
+            CreateItemActionEntity(new_pool[(which % #new_pool) + 1], x2, y2)
+        end
+    end
+end
+
+if bonk then
     GamePlaySound("data/audio/Desktop/ui.bank", "ui/button_denied", x, y)
 end
 
